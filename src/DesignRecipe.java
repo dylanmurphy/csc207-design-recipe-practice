0b067ee9b5786560b5fa5a0219b30b9c8c94a248
Dylan Murphy <murphydy@grinnell.edu>
2015-09-05 11:17:50 -0500

    add test cases

--- src/DesignRecipe.java
+++ src/DesignRecipe.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 
 public class DesignRecipe {
 	public static String[] intersperse(String s, String[] arr) {
@@ -6,13 +8,36 @@ public class DesignRecipe {
 		} else if (arr.length < 2) {
 			return arr;
 		} else {
-			return null;
+			String[] ret = new String[2 * arr.length-1];
+			for (int i = 0; i < arr.length -1; i++) {
+				ret[i*2] = arr[i];
+				ret[2*i+1] = s;
+			}
+			ret[ret.length - 1] = arr[arr.length - 1];
+			return ret;
 		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println(intersperse(null,null));
-		//System.out.println(intersperse("hello", {"1"}));
+		String[] arr1 = new String[1];
+		arr1[0] = "1";
+		String[] empty = {};
+		System.out.println(Arrays.toString(intersperse(null,null)));
+		System.out.println(Arrays.toString(intersperse("hello",null)));
+		System.out.println(Arrays.toString(intersperse(null,arr1)));
+		System.out.println(Arrays.toString(intersperse("hello", arr1)));
+		System.out.println(Arrays.toString(intersperse("hello",empty)));
+		String[] arr2 = new String[2];
+		arr2[0] = "a";
+		arr2[1] = "b";
+		System.out.println(Arrays.toString(intersperse("hello", arr2)));
+		String[] arr3 = new String[3];
+		arr3[0] = "a";
+		arr3[1] = "b";
+		arr3[2] = "c";
+		System.out.println(Arrays.toString(intersperse("hello", arr3)));
+		
+		System.out.println(Arrays.toString(intersperse("hello",empty)));
 	}
 
 }
